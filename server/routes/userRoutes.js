36da09a6ab54afc6f22aec328ad66d93d4c32a83
nahla <znahlakhouloud@gmail.com>
2026-07-30 23:41:40 +0200

    feat(auth): protect routes using JWT authentication middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {fetchUsers,addUser,fetchUserById,editUser,removeUser} from "../controllers/userController.js";
 import {validateUser} from "../middleware/validateUser.js";
-
+import {authenticateToken} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/",fetchUsers);
+router.get("/",authenticateToken,fetchUsers);
 router.post("/",validateUser,addUser);
 router.get("/:id",fetchUserById);
 router.put("/:id",editUser);
